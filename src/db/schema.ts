bd1427a3461dc1e9bf982764a0d384907b1fbcc6
Rik de Goede <rik.dgd@gmail.com>
2025-04-08 00:35:36 +0200

    start adding REST endpoints using express

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,10 @@
-import { int, mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core';
+import { int, mysqlTable as table, varchar, datetime } from 'drizzle-orm/mysql-core';
 
-export const notificationTable = mysqlTable('notifications_table', {
+export const notificationTable = table('notifications_table', {
     id: int().primaryKey().autoincrement(),
     title: varchar({ length: 255 }).notNull(),
     body: varchar({ length: 255 }).notNull(),
     urgency: varchar({ length: 255 }).notNull(),
+    timeReceived: datetime('time_received').notNull(),
+    userId: int('user_id').notNull(),
 });
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,39 @@
+import express from 'express';
 import amqp from 'amqplib/callback_api.js';
 import { handleIncomingMessage } from './message_handling.ts';
 
 
+const app = express();
+const port = 3000;
 const queueName = 'notifications-queue';
 
-if (import.meta.main) {    
-    amqp.connect('amqp://rabbit-mq', (error0, connection) => {
-        if (error0) {
-            throw error0;
+
+app.listen(port, () => {
+    console.log(`Started listening on port ${port}`);
+});
+
+// Connect to the amqp message queue
+amqp.connect('amqp://rabbit-mq', (error0, connection) => {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel((error1, channel) => {
+        if (error1) {
+            throw error1;
         }
-        connection.createChannel((error1, channel) => {
-            if (error1) {
-                throw error1;
+        
+        channel.assertQueue(queueName, {
+            durable: true
+        });
+        channel.prefetch(1);
+        
+        console.log(` [*] Waiting for messages in ${queueName}`);
+        channel.consume(queueName, (msg) => {
+            if (msg) {
+                handleIncomingMessage(msg);
             }
-            
-            channel.assertQueue(queueName, {
-                durable: true
-            });
-            channel.prefetch(1);
-            
-            console.log(` [*] Waiting for messages in ${queueName}`);
-            channel.consume(queueName, (msg) => {
-                if (msg) {
-                    handleIncomingMessage(msg);
-                }
-            }, {
-                noAck: false
-            });
+        }, {
+            noAck: false
         });
     });
-}
+});
